03f68c307373b8f7b76262cf7068212a5f4c121e
Rodrigo Jardim <rodjle@hotmail.com>
2017-08-18 09:55:01 -0300

    Primeiro programa em C#

--- PrimeiroPrograma/PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -8,18 +8,21 @@ namespace PrimeiroPrograma
 {
     public class Program
     {
+        //declaração de uma lista
         public static List<Carro> listaCarro;
 
+        //método principal
         static void Main(string[] args)
         {
+            //criando uma lista vazia de carros
             listaCarro = new List<Carro>();
 
-            //instancia ou criando
+            //criando objetos do tipo carro
             Carro carro1 = new Carro("Ford");
             Carro carro2 = new Carro("GM");
             Carro carro3 = new Carro("Fiat");
 
-            //adicionando
+            //adicionando na lista
             listaCarro.Add(carro1);
             listaCarro.Add(carro2);
             listaCarro.Add(carro3);
@@ -32,36 +35,36 @@ namespace PrimeiroPrograma
 
         }
 
-   
-            /*  foreach (var argumento in args)
-              {
-                  Console.WriteLine("O argumento é:"+argumento);
-              }*/
+
+        /*  foreach (var argumento in args)
+          {
+              Console.WriteLine("O argumento é:"+argumento);
+          }*/
 
 
-            //var meuPrograma = new Program();
-            //int x = 20;
-            //int y = 30;
-            //Console.WriteLine("Antes - Valor de x é {0} e y é {1}", x, y);
+        //var meuPrograma = new Program();
+        //int x = 20;
+        //int y = 30;
+        //Console.WriteLine("Antes - Valor de x é {0} e y é {1}", x, y);
 
-            //meuPrograma.Soma(x, y);
+        //meuPrograma.Soma(x, y);
 
-            //Console.WriteLine("Depois - Valor de x é {0} e y é {1}", x, y);
+        //Console.WriteLine("Depois - Valor de x é {0} e y é {1}", x, y);
 
 
-          /*  var meuPrograma = new Program();
+        /*  var meuPrograma = new Program();
 
-            int total=0 ;
+          int total=0 ;
 
-            int x = 40;
-            int y = 30;
-            Console.WriteLine("Antes - total é:"+total);
+          int x = 40;
+          int y = 30;
+          Console.WriteLine("Antes - total é:"+total);
 
-            meuPrograma.Soma2(x,y, out total);
+          meuPrograma.Soma2(x,y, out total);
 
-            Console.WriteLine("Depois - total é:" + total);*/
+          Console.WriteLine("Depois - total é:" + total);*/
 
-       // }
+        // }
 
 
         public static void ImprimeMsg()
@@ -73,13 +76,13 @@ namespace PrimeiroPrograma
         }
 
         //passagem de parametros por valor
-        public int Soma(int x,int y)
+        public int Soma(int x, int y)
         {
             return x + y;
         }
 
-        //passagem de parametros por valor
-        public void Soma2(int x, int y,out int total)
+        //passagem de parametros por referência
+        public void Soma2(int x, int y, out int total)
         {
             total = x + y;
         }
