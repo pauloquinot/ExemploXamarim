6e6c97e1203fb3f06b6010b779b69fc671049fe6
rodrigo <rodrigo@TI-02>
2017-08-24 19:15:51 -0300

    Exemplo

---
+++ Xamarin-1/Xamarin-1/Xamarin_1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            
+        }
+        public void AoClicar(object sender,EventArgs e)
+        {
+            
+                lbl.Text = "Seu nome é:" + edt.Text;//"Hello:" + DateTime.Now;
+            
+        }
+	}
+}
---
+++ Xamarin-1/Xamarin-1/Xamarin_1/MinhaPagina.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin_1
+{
+    public class MinhaPagina: Xamarin.Forms.ContentPage
+    {
+        Button button;
+        Label lbl;
+        Entry entry;
+        public MinhaPagina()
+        {
+            entry = new Entry
+            {
+                Placeholder = "Digite seu nome aqui",
+                VerticalOptions = LayoutOptions.Center,
+                Keyboard = Keyboard.Text
+            };
+
+            button =
+                    new Button
+                    {
+                        Text = "Clique aqui",
+                        FontSize = 12,
+                        HorizontalOptions = LayoutOptions.Center,
+
+                    };
+
+            button.Clicked += (sender, args) =>
+            {
+                lbl.Text = "Seu nome é:" + entry.Text;//"Hello:" + DateTime.Now;
+            };
+
+            lbl = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.End,
+
+                // YAlign=TextAlignment.Start,
+                Text = "",
+                FontSize = 20,
+                TextColor = Color.White
+            };
+
+
+
+
+            Title = "Meu App em C#";
+            Content = new StackLayout
+            {
+               
+                BackgroundColor =Color.Gray,
+                Padding = 20,
+                VerticalOptions=LayoutOptions.Start,
+               // HeightRequest=500,
+                Children =
+                {
+                    new Label
+                    {
+                        HorizontalTextAlignment=TextAlignment.Start,
+                        
+                       // YAlign=TextAlignment.Start,
+                        Text="Digite Seu Nome",
+                        //FontSize=20,
+                        TextColor=Color.Purple
+                    },
+                    entry,
+                    button,
+                    lbl
+
+                }
+            };
+
+
+            
+        }
+    }
+}
